0852d83b6f4dc00d430ecf73b31d9e5625d03fac
Josh-Wickens <joshwickens@hotmail.com>
2023-01-20 14:47:24 +0000

    add profile detail view so details in profiles can be edited

--- profiles/views.py
+++ profiles/views.py
@@ -13,6 +13,7 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -24,3 +25,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
