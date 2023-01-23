b8137e121fa33aceb7cef97677422eac79670190
Josh-Wickens <joshwickens@hotmail.com>
2023-01-23 14:47:43 +0000

    add pagination and date settings to posts

--- topic_comments/serializers.py
+++ topic_comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import TopicComment
 from likes.models import LikeTopicComment
@@ -16,10 +17,18 @@ class TopicCommentSerializer(serializers.ModelSerializer):
     fan_or_club = serializers.ReadOnlyField(source='owner.profile.fan_or_club')
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     def get_like_id(self, obj):
         user = self.context['request'].user
